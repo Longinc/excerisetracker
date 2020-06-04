65d6d23c67a0cfc9de884ee4213e071a9e6cd780
Billy Long <billylong@Billys-iMac.home>
2020-06-04 14:07:03 -0400

    Updated code

--- DOM event practice/js/file.js
+++ DOM event practice/js/file.js
@@ -1,16 +1,69 @@
-let button1 = document.getElementById("btn1");
-let exerciseInput = document.getElementById("name");
-let caloriesInput = document.getElementById("quantity");
-let button2 = document.getElementById('btn2');
-let ul = document.getElementById('ul');
-let exerciseName =  exerciseInput.value;
-let caloriesQuantitiy = caloriesInput.value;
-
-button1.addEventListner("click", function (exerciseName, caloriesQuantity)  {
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(exerciseName + caloriesQuantity));
-    ul.appendChild(li);
-    caloriesInput.value && exerciseInput.value = "";
+var addButton = document.getElementById("btn1");
+var clearButton = document.getElementById("btn2");
+var name = document.getElementById("name");
+var calories = document.getElementById("quantity");
+var ul = document.getElementById("ul");
+var ul2 = document.getElementById("ul2");
 
 
-})
+//function addLi2() {
+    //var calories = document.getElementById("quantity")
+   // var li2 = document.createElement("li2");
+   // li2.appendChild(document.createTextNode(quantity.value));
+   // ul2.appendChild(li2);
+   // name.value = "";
+//}
+
+//function addLi() {
+   // var name = document.getElementById("name");
+   // var li = document.createElement("li");
+       // li.appendChild(document.createTextNode(name.value));
+  //  ul.appendChild(li);
+   // name.value = "";
+//}
+
+addButton.addEventListener("click", function (e)  {
+    e.preventDefault()
+    var name = document.getElementById("name");
+    var calories = document.getElementById("quantity")
+
+
+
+    if (name.value.length > 0 && quantity.value > 0) {
+        //var name = document.getElementById("name");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(name.value));
+        ul.appendChild(li);
+        name.value = ""
+
+
+        var li2 = document.createElement("li");
+        li2.appendChild(document.createTextNode(calories.value));
+        ul2.appendChild(li2);
+        calories.value = "";
+        }
+
+});
+
+
+clearButton.addEventListener("click", function (e) {
+   e.preventDefault()
+
+    var name = document.getElementById("name");
+    var ul = document.getElementById("ul");
+    var name = document.getElementById("quantity");
+    var ul2 = document.getElementById("ul2");
+
+
+
+    while (ul.lastElementChild) {
+        ul.removeChild(ul.lastElementChild);
+    }
+    while (ul2.lastElementChild) {
+        ul2.removeChild(ul2.lastElementChild);
+    }
+
+    name.value = "";
+    quantity.value = "";
+});
+
